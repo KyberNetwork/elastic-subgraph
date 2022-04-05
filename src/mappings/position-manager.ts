fd3141421b81eaa78281fa4c3e0217f392d4cba3
viet-nv <vietnv1304@gmail.com>
2022-04-05 16:51:15 +0700

    feat: listen add/remove liquidity event

--- src/mappings/position-manager.ts
+++ src/mappings/position-manager.ts
@@ -4,11 +4,12 @@ import {
   AddLiquidity,
   RemoveLiquidity,
   AntiSnipAttackPositionManager,
-  Transfer
+  Transfer,
+  MintPosition
 } from '../types/AntiSnipAttackPositionManager/AntiSnipAttackPositionManager'
 import { Bundle, Position, PositionSnapshot, Token } from '../types/schema'
 import { ADDRESS_ZERO, factoryContract, ZERO_BD, ZERO_BI } from '../utils/constants'
-import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts'
+import { Address, BigInt, ethereum, log } from '@graphprotocol/graph-ts'
 import { convertTokenToDecimal, loadTransaction } from '../utils'
 
 function getPosition(event: ethereum.Event, tokenId: BigInt): Position | null {
@@ -84,18 +85,47 @@ function savePositionSnapshot(position: Position, event: ethereum.Event): void {
   positionSnapshot.save()
 }
 
+export function handleMintPosition(event: MintPosition): void {
+  let position = getPosition(event, event.params.tokenId)
+
+  if (position == null) {
+    return
+  }
+
+  let bundle = Bundle.load('1')
+
+  let token0 = Token.load(position.token0)
+  let token1 = Token.load(position.token1)
+
+  let amount0 = convertTokenToDecimal(event.params.amount0, token0.decimals)
+  let amount1 = convertTokenToDecimal(event.params.amount1, token1.decimals)
+
+  position.liquidity = position.liquidity.plus(event.params.liquidity)
+  position.depositedToken0 = position.depositedToken0.plus(amount0)
+  position.depositedToken1 = position.depositedToken1.plus(amount1)
+
+  let newDepositUSD = amount0
+    .times(token0.derivedETH.times(bundle.ethPriceUSD))
+    .plus(amount1.times(token1.derivedETH.times(bundle.ethPriceUSD)))
+  position.amountDepositedUSD = position.amountDepositedUSD.plus(newDepositUSD)
+
+  updateFeeVars(position!, event, event.params.tokenId)
+
+  position.save()
+
+  savePositionSnapshot(position!, event)
+}
+
 export function handleIncreaseLiquidity(event: AddLiquidity): void {
   let position = getPosition(event, event.params.tokenId)
+
+  log.debug('vietnv {} {}', [event.params.liquidity.toString(), event.params.tokenId.toString()])
 
   // position was not able to be fetched
   if (position == null) {
     return
   }
 
-  // temp fix
-  if (Address.fromString(position.pool).equals(Address.fromHexString('0x8fe8d9bb8eeba3ed688069c3d6b556c9ca258248'))) {
-    return
-  }
   let bundle = Bundle.load('1')
 
   let token0 = Token.load(position.token0)
@@ -125,11 +155,6 @@ export function handleDecreaseLiquidity(event: RemoveLiquidity): void {
 
   // position was not able to be fetched
   if (position == null) {
-    return
-  }
-
-  // temp fix
-  if (Address.fromString(position.pool).equals(Address.fromHexString('0x8fe8d9bb8eeba3ed688069c3d6b556c9ca258248'))) {
     return
   }
 
--- src/utils/constants.template.ts
+++ src/utils/constants.template.ts
@@ -3,13 +3,13 @@ import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5b5371b273a09a3abb9a8565c763118fbd512cc4'
+export const FACTORY_ADDRESS = '{{ factory_address }}'
 
-export const WETH_ADDRESS = '0xae13d989dac2f0debff460ac112a837c89baa7cd'
-export const USDC_WETH_03_POOL = '0x76610541a0721c566c45cf26a34c3b280ea6f894'
+export const WETH_ADDRESS = '{{ wrapped_native_address }}'
+export const USDC_WETH_03_POOL = '{{ pricing_pools }}'
 
-export const STABLE_COINS_STR = '0x50a8c32cca7edcb2104bfc6bd7b50fa4dc6b6673|0x102e969a101d0f5a7defb29b4090c08f8def0f42|0x74c21a9e797ef95066c33ba0cc295ca67a7f6d18'
-export const WHITELIST_TOKENS_STR = '0x51e8d106c646ca58caf32a47812e95887c071a62'
+export const STABLE_COINS_STR = '{{ stable_coins }}'
+export const WHITELIST_TOKENS_STR = '{{ whitelist_tokens }}'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
