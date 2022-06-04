d411961454ee9ba808e55edaaddf83c8778bdbd6
Nguyen Van Viet <vietnv1304@gmail.com>
2022-06-04 10:05:20 +0700

    add bsc config

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,13 +3,13 @@ import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xbc1a68889eb9de88838259b16d30c3639304a546'
+export const FACTORY_ADDRESS = '0x2c059ab112eb6d4bdb6105742d560876a9fe0f54'
 
-export const WETH_ADDRESS = '0xc778417e063141139fce010982780140aa0cd5ab'
-export const PRICING_POOL = '0x53b53716b20aa2b6f264ba76f4423adcc306305b'
+export const WETH_ADDRESS = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
+export const PRICING_POOL = '0x2d0fe0ac602db6a484453992a60134e9fd636760'
 
-export const STABLE_COINS_STR = '0xc7ad46e0b8a400bb3c915120d284aafba8fc4735|0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b'
-export const WHITELIST_TOKENS_STR = '0x01be23585060835e02b77ef475b0cc51aa1e0709|0x6f072ce36c65fd5a1f2534e5598e43b9d2fa7c77|0x07efdb214ee4260f3351e1048766fcd225e6e25e'
+export const STABLE_COINS_STR = '0xe9e7cea3dedca5984780bafc599bd69add087d56|0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d|0x55d398326f99059ff775485246999027b3197955|0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3'
+export const WHITELIST_TOKENS_STR = '0xfe56d5892bdffc7bf58f2e84be1b2c32d21c308b|0x2170ed0880ac9a755fd29b2688956bd959f933f8'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
